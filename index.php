7d704fc2a45d355d2688f0f504b2cc7aa4a60262
giadinhkhaiha <149668041+giadinhkhaiha@users.noreply.github.com>
2023-11-02 20:16:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investment Proposal</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Investment Proposal</h1>
+        
+        <p>Welcome to our clothing business! We are a company with 90 employees and are currently seeking investment to further grow our operations.</p>
+        
+        <h2>About Us</h2>
+        <p>Our company specializes in the fashion industry, providing high-quality clothing to our customers. With a dedicated team of 90 employees, we have successfully established ourselves in the market.</p>
+
+        <h2>Why Invest in Us?</h2>
+        <p>We have a proven track record of delivering exceptional products and have identified strategic opportunities for expansion. Your investment will play a crucial role in supporting our growth initiatives and taking our business to new heights.</p>
+
+        <h2>Contact Information</h2>
+        <p>If you are interested in discussing investment opportunities with us, please feel free to contact us:</p>
+        <ul>
+            <li>Email: info@yourcompany.com</li>
+            <li>Phone: +1 (555) 123-4567</li>
+        </ul>
+    </div>
+</body>
+</html>
